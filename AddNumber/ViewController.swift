41e50ffc21991b36b550a498faedae783079ce18
Juyel Rana <juyelcse11th@gmail.com>
2019-04-18 02:07:24 +0600

    second commit

--- AddNumber/ViewController.swift
+++ AddNumber/ViewController.swift
@@ -14,11 +14,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var num2: UITextField!
     
     @IBOutlet weak var output: UILabel!
+
+    @IBOutlet weak var action: UISwitch!
     
+    @IBAction func actionSwitch(_ sender: UISwitch) {
+        if action.isOn == true{
+            output.text = "On"
+            view.backgroundColor = UIColor.brown
+        }else{
+            output.text = "Off"
+            view.backgroundColor = UIColor.green
+        }
+    }
     @IBAction func btnAdd(_ sender: Any) {
         let a = Int(num1.text!)
         let b = Int(num2.text!)
-        
+    
         output.text = "\(a!+b!)" 
         
     }
